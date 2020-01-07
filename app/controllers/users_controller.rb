d0bede8873c971a30dfefb02f8bb7897e77576a1
Daniel Lagos <lagos.d@gmail.com>
2020-01-06 20:31:23 -0800

    back to where the helpers are added.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-<<<<<<< HEAD
+
   before_action :set_user, only: [:edit, :update, :show]
-=======
->>>>>>> parent of ffbfa2b... add helper methods for authentication system.
-=======
->>>>>>> parent of ffbfa2b... add helper methods for authentication system.
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end 
@@ -26,11 +22,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -40,7 +35,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -49,4 +43,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
