96ab6e3b19f60b6bdec9015eeeea2629866900df
Daniel Lagos <lagos.d@gmail.com>
2020-01-06 19:21:30 -0800

    bringing back to the point before I tried to fix drop down
    bringing back to the point before I tried to fix drop down

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+<<<<<<< HEAD
   before_action :set_user, only: [:edit, :update, :show]
+=======
+>>>>>>> parent of ffbfa2b... add helper methods for authentication system.
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end 
@@ -20,11 +23,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
       redirect_to articles_path
@@ -34,7 +37,7 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -42,7 +45,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  def set_user
-    @user = User.find(params[:id])
-  end
+
 end
