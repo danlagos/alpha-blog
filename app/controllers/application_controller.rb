e3f5aadde02e6766ecf4b96796d0c0d01eb533f1
Daniel Lagos <lagos.d@gmail.com>
2020-01-07 20:50:03 -0800

    complete authentication system.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def require_user
     if !logged_in?
-      flash[:danger] = "You must be logged in to perfomr that action"
+      flash[:danger] = "You must be logged in to perform that action"
       redirect_to root_path
     end
   end
